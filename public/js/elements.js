eea378588f5b78e5870fb5dd48699a288a8a2cc5
shafarizkyf <shafarizkyf@gmail.com>
2025-07-09 13:41:46 +0700

    add select product to import from the list

--- public/js/elements.js
+++ public/js/elements.js
@@ -1,10 +1,16 @@
-const ProductCardEl = ({ imageUrl, title, price, viewUrl }) => {
+const ImportProductCardEl = ({ imageUrl, title, price, viewUrl, index }) => {
   return `
     <div class="card" style="width: 200px;">
       <img src="${imageUrl}" class="card-img-top" alt="${title}">
       <div class="card-body">
         <h5 class="card-title text-ellipsis">${title}</h5>
         <p class="card-text">${price}</p>
+        <div class="form-check">
+          <input class="form-check-input" type="checkbox" value="" id="item-${index}" checked>
+          <label class="form-check-label" for="item-${index}">
+            Selected
+          </label>
+        </div>
       </div>
     </div>
   `
--- public/js/product-import.js
+++ public/js/product-import.js
@@ -1,16 +1,18 @@
 (function(){
   let file = null;
+  let fileContent = null;
   const readFile = (file) => {
     const reader = new FileReader();
     reader.onload = function(e) {
       const text = e.target.result;
-      const { data: products } = JSON.parse(text);
+      fileContent = JSON.parse(text);
 
-      const previewCards = products
-        .map(product => ProductCardEl({
+      const previewCards = fileContent.data
+        .map((product, i) => ImportProductCardEl({
           imageUrl: product.imageUrl,
           price: product.discountPrice || product.normalPrice,
-          title: product.name
+          title: product.name,
+          index: i
         }))
         .join('');
 
@@ -50,5 +52,31 @@
     document.getElementById('preview-container').innerHTML = "";
     document.getElementById('btn-import').classList.add('d-none');
     this.classList.add('d-none');
+  });
+
+  document.getElementById('btn-import').addEventListener('click', function(e){
+    e.preventDefault();
+    const selectedProducts = [];
+    const unselectedIndex = []
+    document.querySelectorAll('#preview-container input[type="checkbox"]').forEach((el, i) => {
+      if (!el.checked) {
+        unselectedIndex.push(i);
+      }
+    });
+
+    fileContent.data.forEach((product, index) => {
+      if (unselectedIndex.indexOf(index) === -1) {
+        selectedProducts.push(product);
+      }
+    });
+
+    const updatedFileContent = { ...fileContent }
+    updatedFileContent.data = selectedProducts;
+
+    const updatedFile = new File([updatedFileContent], "products.json", {
+      type: 'application/json'
+    });
+
+    console.log(updatedFileContent, updatedFile);
   });
 })();
