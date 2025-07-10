f2332f06c2c37a54d28eb03d5160c74bfa660ce0
shafarizkyf <shafarizkyf@gmail.com>
2025-07-10 15:17:38 +0700

    init sidebar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,8 +9,11 @@
   @yield('css')
 </head>
 <body>
-  <main>
-    @yield('content')
+  <main class="d-flex">
+    @include('components.sidebar')
+    <div id="content-wrapper" class="bg-light">
+      @yield('content')
+    </div>
   </main>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
   <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
