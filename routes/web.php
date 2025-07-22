dc2f50d121d57bb8f477b070e8d8d87998dc67f5
shafarizkyf <shafarizkyf@gmail.com>
2025-07-22 13:40:48 +0700

    fix route priority

--- routes/web.php
+++ routes/web.php
@@ -8,11 +8,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('', [HomepageController::class, 'index']);
 
 Route::prefix('products')->group(function(){
-  Route::get('', [ProductController::class, 'index']);
+  Route::get('', [ProductController::class, 'index'])->name('products.index');
   Route::get('add', [ProductController::class, 'add'])->name('products.add');
   Route::get('{product}/edit', [ProductController::class, 'edit']);
-  Route::get('{productSlug}', [ProductController::class, 'show']);
   Route::get('import', [ProductController::class, 'import'])->name('products.import');
+  Route::get('{productSlug}', [ProductController::class, 'show']);
 });
 
 Route::get('image-downloader', [ImageDownloadController::class, 'download']);
