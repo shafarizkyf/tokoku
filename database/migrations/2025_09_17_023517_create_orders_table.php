ee8410007e9bd47a8ef58bb6b0d8dfc3fe62af5c
shafarizkyf <shafarizkyf@gmail.com>
2025-09-17 09:51:12 +0700

    init order migration

---
+++ database/migrations/2025_09_17_023517_create_orders_table.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->string('code', 20)->unique();
+            $table->enum('status', ['pending', 'paid', 'shipped', 'completed', 'cancelled'])->default('pending');
+            $table->string('payment_method', 20);
+            $table->enum('payment_status', ['unpaid', 'paid', 'expired', 'failed'])->default('unpaid');
+            $table->text('payment_response')->nullable();
+            $table->dateTime('payment_expired_at');
+            $table->decimal('total_price', 12);
+            $table->unsignedMediumInteger('total_weight');
+            $table->decimal('shipping_price', 12);
+            $table->decimal('grand_total', 12);
+            $table->string('courier', 50);
+            $table->string('resi_number', 50);
+            $table->string('recipient_name', 50);
+            $table->string('recipient_phone', 20);
+            $table->text('address_detail');
+            $table->unsignedMediumInteger('province_id');
+            $table->unsignedInteger('regency_id');
+            $table->unsignedInteger('district_id');
+            $table->unsignedInteger('village_id');
+            $table->unsignedInteger('postal_code');
+            $table->text('note')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
---
+++ database/migrations/2025_09_17_024835_create_order_details_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('order_details', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('order_id');
+            $table->unsignedBigInteger('product_id');
+            $table->unsignedBigInteger('variation_id');
+            $table->string('name_snapshot');
+            $table->decimal('price');
+            $table->unsignedMediumInteger('quantity');
+            $table->unsignedMediumInteger('weight');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('order_details');
+    }
+};
