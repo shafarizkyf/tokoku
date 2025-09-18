ce364162c2e0c298c1db0a1aa1ef2c2ae8488bd3
shafarizkyf <shafarizkyf@gmail.com>
2025-09-18 14:32:56 +0700

    revert redirect

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -2,19 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Cart;
-use Illuminate\Http\Request;
-
 class CartController extends Controller {
 
   public function index() {
-    $sessionId = request()->cookie('session_id');
-    $cart = Cart::whereSessionId($sessionId)->first();
-
-    if (!$cart || ($cart && !$cart->items->count())) {
-      return redirect()->back()->with('message', 'Keranjang anda kosong. Silahkan berbelanja terlebih dahulu');
-    }
-
     return view('homepage.cart.index');
   }
 
