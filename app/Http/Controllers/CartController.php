763b50b2d618360398993ed315e889a61cc94173
shafarizkyf <shafarizkyf@gmail.com>
2025-09-18 12:52:19 +0700

    prevent to go to cart page when empty cart

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -8,6 +8,13 @@ use Illuminate\Http\Request;
 class CartController extends Controller {
 
   public function index() {
+    $sessionId = request()->cookie('session_id');
+    $cart = Cart::whereSessionId($sessionId)->first();
+
+    if (!$cart || ($cart && !$cart->items->count())) {
+      return redirect()->back()->with('message', 'Keranjang anda kosong. Silahkan berbelanja terlebih dahulu');
+    }
+
     return view('homepage.cart.index');
   }
 
