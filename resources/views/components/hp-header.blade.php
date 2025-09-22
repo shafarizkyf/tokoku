804c096b48ca67aecc8eb03886ed71a065003df8
shafarizkyf <shafarizkyf@gmail.com>
2025-09-22 21:22:16 +0700

    load app name from env

--- resources/views/components/hp-header.blade.php
+++ resources/views/components/hp-header.blade.php
@@ -1,7 +1,7 @@
 <header>
   <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-light shadow-lg">
     <div class="container">
-      <a class="navbar-brand text-dark" href="{{ url('/') }}">TokoKu</a>
+      <a class="navbar-brand text-dark" href="{{ url('/') }}">{{ config('app.name') }}</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
