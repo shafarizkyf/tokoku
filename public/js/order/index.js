5b6f7df4ff6f7b26d868b1a48d549c797f900c50
shafarizkyf <shafarizkyf@gmail.com>
2025-09-24 20:27:00 +0700

    handle update resi number

--- public/js/order/index.js
+++ public/js/order/index.js
@@ -1,5 +1,7 @@
 $(function(){
 
+  let orderId;
+
   const BAGDE_CLASS = {
     pending: 'text-bg-warning',
     paid: 'text-bg-success',
@@ -7,7 +9,7 @@ $(function(){
     cancelled: 'text-bg-danger'
   }
 
-  $('#table-orders').DataTable({
+  const table = $('#table-orders').DataTable({
     serverSide: true,
     ordering: false,
     ajax: {
@@ -29,11 +31,11 @@ $(function(){
             `
               <div class="d-flex justify-content-end">
                 <div class="btn-group" role="group">
-                  <a href="/orders/${item.code}" class="btn btn-primary btn-sm">Lihat Detail Transaksi</a>
+                  <a href="/orders/${item.code}" class="btn btn-dark btn-sm">Lihat Detail Transaksi</a>
                   <div class="btn-group" role="group">
-                    <button type="button" class="btn btn-primary btn-sm dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false"></button>
+                    <button type="button" class="btn btn-dark btn-sm dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false"></button>
                     <ul class="dropdown-menu">
-                      <li><a class="dropdown-item" href="#">Atur Pengiriman</a></li>
+                      <li><button name="btn-show-resi-number" class="dropdown-item" data-id="${item.id}" data-resi-number="${item.resi_number}">Atur Pengiriman</a></li>
                       <li><button class="dropdown-item text-danger" href="#">Batalkan Order</button></li>
                     </ul>
                   </div>
@@ -62,6 +64,29 @@ $(function(){
         targets: 5
       },
     ]
-  })
+  });
+
+  $(document).on('click', 'button[name="btn-show-resi-number"]', function(e){
+    e.preventDefault();
+    orderId = $(this).data('id');
+    const resiNumber = $(this).data('resi-number');
+
+    $('#resi_number').val(resiNumber);
+    $('#resiNumberModal').modal('show');
+  });
+
+  $('button[name="btn-save-resi-number"]').on('click', function(e){
+    e.preventDefault();
+    const data = {
+      _method: 'PATCH',
+      resi_number: $('#resi_number').val(),
+    }
+
+    $.post(`/api/orders/${orderId}/resi-number`, data).then(response => {
+      $('#resiNumberModal').modal('hide');
+      toast({ text: response.message });
+      table.ajax.reload();
+    });
+  });
 
 });
