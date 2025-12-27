75d9faddb6649aa5441578eb5dcd380b5847dedb
shafarizkyf <shafarizkyf@gmail.com>
2025-12-28 05:37:13 +0700

    add crud banner test

--- app/Models/Banner.php
+++ app/Models/Banner.php
@@ -2,10 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Storage;
 
 class Banner extends Model {
+
+  use HasFactory;
 
   protected $fillable = [
     'path',
