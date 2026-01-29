dd2ab754a3d121420749b235e389a1acb99efde3
shafarizkyf <shafarizkyf@gmail.com>
2026-01-29 08:15:58 +0700

    fix redeclare method

--- app/Http/Controllers/API/TripayController.php
+++ app/Http/Controllers/API/TripayController.php
@@ -39,54 +39,6 @@ class TripayController extends Controller {
     ];
   }
 
-  protected function handleOrderCallback(Order $order) {
-    Log::channel('tripay')->info(request('status'), ['order_id' => $order->id]);
-    $paymentResponse = $order->payment_response;
-    $paymentResponse->callback = request()->all();
-    $order->payment_response = json_encode($paymentResponse);
-    $status = strtoupper(request('status'));
-
-    switch ($status) {
-      case 'PAID':
-        $order->status = 'paid';
-        $order->payment_status = 'paid';
-        $order->paid_at = now();
-        $order->save();
-
-        // email receipt
-        $emailTemplate = view('email.order', compact('order'))->render();
-        Gmail::send($order->user->email, config('app.name'). ": Kuitansi Pesanan", $emailTemplate);
-
-        // send notification to admin
-        $amount = Utils::currencyFormat($order->grand_total);
-        $notificationMessage = "Pesanan {$order->code} senilai: {$amount} telah dibayar." ;
-        WhatsApp::sendText($notificationMessage);
-      break;
-
-      case 'FAILED':
-        $order->status = 'pending';
-        $order->payment_status = 'failed';
-        $order->save();
-
-        Stock::revert($order);
-      break;
-
-      case 'EXPIRED':
-        $order->status = 'pending';
-        $order->payment_status = 'expired';
-        $order->save();
-
-        Stock::revert($order);
-      break;
-
-      case 'REFUND':
-        // TODO:
-      break;
-    }
-
-    return [];
-  }
-
   protected function handleOrderCallback(Order $order): void
   {
     Log::channel('tripay')->info(request('status'), ['order_id' => $order->id]);
